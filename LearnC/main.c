dc656a8be660892127caa82b909b6ff4d14f65a5
이시바 <jinho971031@gmail.com>
2023-02-17 01:01:28 +0900

    Initial Commit

---
+++ LearnC/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LearnC
+//
+//  Created by 김진호 on 2023/02/17.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
